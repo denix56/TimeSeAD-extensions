134cfc311b8014c721a90bb436b97edffd166ad8
Denys Senkin <denisx9.0c@gmail.com>
2026-01-22 19:34:16 +0100

    Add group transform with time warp and scaling (#8)

--- timesead_ext/models/transforms/__init__.py
+++ timesead_ext/models/transforms/__init__.py
@@ -1,4 +1,14 @@
 from .base import Transform, TransformBank
+from .group import GroupTransform, group_cfg, make_group_family
 from .invertible import InvertibleFlow, invertible_cfg, make_invertible_family
 
-__all__ = ["Transform", "TransformBank", "InvertibleFlow", "invertible_cfg", "make_invertible_family"]
+__all__ = [
+    "Transform",
+    "TransformBank",
+    "GroupTransform",
+    "group_cfg",
+    "make_group_family",
+    "InvertibleFlow",
+    "invertible_cfg",
+    "make_invertible_family",
+]
